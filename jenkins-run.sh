85e951197e0153dd02d4614d599035df72e42d9c
Ejber Ozkan <ejber.ozkan@polestarglobal.com>
2015-09-25 15:20:42 +0100

    *sigh*

--- jenkins-run.sh
+++ jenkins-run.sh
@@ -4,5 +4,5 @@ bash /etc/profile.d/rvm.sh
 export PATH=$PATH:/usr/local/rvm/gems/ruby-2.1.0/bin:/usr/local/rvm/gems/ruby-2.1.0@global/bin:/usr/local/rvm/rubies/ruby-2.1.0/bin:/usr/lib64/qt-3.3/bin:/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/usr/pgsql-9.2/bin:/usr/local/rvm/bin:/root/bin
 
 echo "Jenkins install of slate (Rake/Ruby)"
-
+bundle install
 rake build

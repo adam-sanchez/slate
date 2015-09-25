a82a877f4c15b1afeb830e9fdefa113792bd3513
Ejber Ozkan <ejber.ozkan@polestarglobal.com>
2015-09-25 15:15:45 +0100

    path fix

--- jenkins-run.sh
+++ jenkins-run.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+export PATH=$PATH:/usr/local/rvm/gems/ruby-2.1.0/bin:/usr/local/rvm/gems/ruby-2.1.0@global/bin:/usr/local/rvm/rubies/ruby-2.1.0/bin:/usr/lib64/qt-3.3/bin:/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/usr/pgsql-9.2/bin:/usr/local/rvm/bin:/root/bin
 
 echo "Jenkins install of slate (Rake/Ruby)"
 rake build

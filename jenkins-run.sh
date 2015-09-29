ce7d5e80d5cf2a1ec9938548d194fdf6eec62b31
Ejber Ozkan <ejber.ozkan@polestarglobal.com>
2015-09-29 12:04:28 +0100

    For some reaosn text/css is not identified by s3cmd so force it and split up other folders in build folder

--- jenkins-run.sh
+++ jenkins-run.sh
@@ -11,11 +11,20 @@ export S3KEY=`cat /opt/aws/aws_key`
 export S3SECRET=`cat /opt/aws/aws_secret_key`
 export S3PATH="/slate-latest/"
 
-# s3cmd is installed via pip install s3cmd
+# s3cmd is installed via pip install s3cmd , s3cmd does not seem to set text/css so forcing it for each sub dir
+echo "=-=-=-=-=-=-=-=-="
 echo "s3 put started"
-s3cmd -r -P -M put build/ s3://login-development-polestar-internal-com/slate-latest/ --access_key=$S3KEY --secret_key=$S3SECRET
+s3cmd -r -P -m "text/css" put build/stylesheets/ s3://login-development-polestar-internal-com/slate-latest/stylesheets/ --access_key=$S3KEY --secret_key=$S3SECRET
+s3cmd -r -P -M put build/iamges/ s3://login-development-polestar-internal-com/slate-latest/images/ --access_key=$S3KEY --secret_key=$S3SECRET
+s3cmd -r -P -M put build/fonts/ s3://login-development-polestar-internal-com/slate-latest/fonts/ --access_key=$S3KEY --secret_key=$S3SECRET
+s3cmd -r -P -M put build/javascripts/ s3://login-development-polestar-internal-com/slate-latest/javascripts/ --access_key=$S3KEY --secret_key=$S3SECRET
+s3cmd -P -M put build/index.html s3://login-development-polestar-internal-com/slate-latest/index.html --access_key=$S3KEY --secret_key=$S3SECRET
 echo "s3 put completed"
+echo "=-=-=-=-=-=-=-=-="
 echo "s3 list uploaded files:"
+echo "=-=-=-=-=-=-=-=-="
 s3cmd ls s3://login-development-polestar-internal-com/slate-latest/ --access_key=$S3KEY --secret_key=$S3SECRET
 echo "s3 info:"
+echo "=-=-=-=-=-=-=-=-="
 s3cmd info s3://login-development-polestar-internal-com/slate-latest/ --access_key=$S3KEY --secret_key=$S3SECRET
+echo "=-=-=-=-=-=-=-=-="
